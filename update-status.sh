ef016c5222cfaf88a2f4082ad86bd15481d09aae
greg <greg@server1.bcnode.io>
2026-06-29 01:30:14 +0000

    Clean status generator and add crawler policy

--- update-status.sh
+++ update-status.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
+set -euo pipefail
 
-CONF="/var/lib/bitcoind/.bitcoin/bitcoin.conf"
-OUT="/var/www/bcnode.io/status.json"
-TMP=$(mktemp)
-
-BLOCKCHAIN=$(sudo -u bitcoin bitcoin-cli -conf="$CONF" getblockchaininfo)
-NETWORK=$(sudo -u bitcoin bitcoin-cli -conf="$CONF" getnetworkinfo)
-MEMPOOL=$(sudo -u bitcoin bitcoin-cli -conf="$CONF" getmempoolinfo)
-UPTIME=$(sudo -u bitcoin bitcoin-cli -conf="$CONF" uptime)
-
-jq -n \
-  --argjson b "$BLOCKCHAIN" \
-  --argjson n "$NETWORK" \
-  --argjson m "$MEMPOOL" \
-  --arg uptime "$UPTIME" \
-'{
-  chain: $b.chain,
-  blocks: $b.blocks,
-  headers: $b.headers,
-  verificationprogress: $b.verificationprogress,
-  initialblockdownload: $b.initialblockdownload,
-  pruned: $b.pruned,
-  connections: $n.connections,
-  version: $n.subversion,
-  mempool_tx: $m.size,
-  mempool_bytes: $m.bytes,
-  uptime_seconds: ($uptime | tonumber)
-}' > "$TMP" && sudo mv "$TMP" "$OUT"
-sudo chmod 644 "$OUT"
+# Canonical bcnode.io status generator.
+# Public site root is /srv/sites/bcnode.io via Caddy.
+exec /usr/local/bin/bcnode-status-json
